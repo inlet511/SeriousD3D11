0cbd9bd25e2d4146c94ea1074fc32621e5c16b87
KenAn <80669349@qq.com>
2022-01-03 22:06:59 +0800

    Constant Buffer

---
+++ 03-ConstantBuffer/hw3d/VertexShader.hlsl
@@ -0,0 +1,18 @@
+struct v2f
+{
+    float3 color : COLOR;
+    float4 pos : SV_Position;
+};
+
+cbuffer CBuf
+{
+    row_major matrix transform;
+};
+
+v2f main( float2 pos : POSITION , float3 color : COLOR)
+{
+    v2f o;
+    o.pos = mul(float4(pos, 0, 1.0f), transform);
+    o.color = color;
+    return o;
+}
